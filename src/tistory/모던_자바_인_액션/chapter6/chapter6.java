c651a1fc1d6035c8c1c40db479d53329f3aab36c
fullcrescent <fullcrescent@naver.com>
2022-07-22 22:53:56 +0900

    2022-07-22 작성중

--- src/tistory/모던_자바_인_액션/chapter6/chapter6.java
+++ src/tistory/모던_자바_인_액션/chapter6/chapter6.java
@@ -1,7 +1,9 @@
 package tistory.모던_자바_인_액션.chapter6;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,14 +12,36 @@ public class chapter6 {
 	public static void main(String[] args) {
 		System.out.println("\n>> 6.1 컬렉터란 무엇인가?");
 		
-		List<String> list = Arrays.asList("User1", "User3", "User4" , "User2", "User111");
-		Stream<String> listStream = list.stream().filter(s -> s.length()==5);
+		List<String> stringList = Arrays.asList("User1", "User3", "User4" , "User2", "User111");
+		Stream<String> listStream = stringList.stream().filter(s -> s.length()==5);
 		List<String> filterList = listStream.collect(Collectors.toList());
 		System.out.println(filterList);
 		
 
 		System.out.println("\n>> 6.2 리듀싱과 요약");
 		
+		stringList = Arrays.asList("User1", "User3", "User4" , "User2", "User111");
+		Long count;
+		
+		count = stringList.stream().collect(Collectors.counting());
+		System.out.println(count);
+		
+		count = stringList.stream().count();
+		System.out.println(count);
+		
+		
+		List<Integer> integerList = Arrays.asList(1, 2, 5, 6, 3, 4);
+		Comparator<Integer> comp = Integer::compareTo;
+		Optional<Integer> optionalInteger;
+		
+		optionalInteger = integerList.stream().collect(Collectors.maxBy(comp));
+		System.out.println(optionalInteger);
+		
+		optionalInteger = integerList.stream().collect(Collectors.minBy(comp));
+		System.out.println(optionalInteger);
+		
+		int total = integerList.stream().collect(Collectors.summingInt(Integer::intValue));
+		System.out.println(total);
 		
 		
 		System.out.println("\n>> 6.3 그룹화");
