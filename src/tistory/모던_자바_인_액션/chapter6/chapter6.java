b1801aaad1fb911ed60490830e6cf8cab8e0f16f
김승곤 <fullcrescent@naver.com>
2022-07-26 22:08:06 +0900

    2022-07-26 작성중

--- src/tistory/모던_자바_인_액션/chapter6/chapter6.java
+++ src/tistory/모던_자바_인_액션/chapter6/chapter6.java
@@ -2,12 +2,21 @@ package tistory.모던_자바_인_액션.chapter6;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class chapter6 {
@@ -75,13 +84,38 @@ public class chapter6 {
 			)
 		);
 		
+		// 그룹화
 		Map<String, List<User>> userGroupTypeMap;
-		userGroupTypeMap = userList.stream().collect(Collectors.groupingBy(User::getType));
+		userGroupTypeMap = userList.stream().collect(Collectors.groupingBy(User::getType));		
 		System.out.println(userGroupTypeMap);				// {usr=[User2, User5], emp=[User0, User1, User3], adm=[User4]}
 		
+		// 다수준 그룹화
+		Map<String, Map<String, List<User>>> userGroupTypeAgeMap;
+		userGroupTypeAgeMap = userList.stream().collect(Collectors.groupingBy(User::getType, 
+															Collectors.groupingBy(user -> {
+																		if(user.getAge()>=30) {
+																			return user.getType() + "MoreThan30";
+																		}else {
+																			return user.getType() + "Under30";
+																		}
+																	})));
+		System.out.println(userGroupTypeAgeMap);
+		// {usr={usrMoreThan30=[User5], usrUnder30=[User2]}, emp={empUnder30=[User0, User1], empMoreThan30=[User3]}, adm={admMoreThan30=[User4]}}
+		
+		// 서브 그룹으로 데이터 수집
 		Map<String, Long> userGroupTypeCountMap;
 		userGroupTypeCountMap = userList.stream().collect(Collectors.groupingBy(User::getType, Collectors.counting()));
 		System.out.println(userGroupTypeCountMap);			// {usr=2, emp=3, adm=1}
+		
+		userGroupTypeCountMap = userList.stream().collect(Collectors.groupingBy(User::getType, 
+															Collectors.summingLong(User::getAge)));
+		System.out.println(userGroupTypeCountMap);			// {usr=70, emp=40, adm=40}
+		
+		Map<String, User> userGroupTypeMaxMap;
+		userGroupTypeMaxMap = userList.stream().collect(Collectors.groupingBy(User::getType, 
+															Collectors.collectingAndThen(Collectors.maxBy(
+																	Comparator.comparingLong(User::getAge)), Optional::get)));
+		System.out.println(userGroupTypeMaxMap);			// {usr=User5, emp=User3, adm=User4}
 		
 		
 		System.out.println("\n>> 6.4 분할");
@@ -109,12 +143,57 @@ public class chapter6 {
 		System.out.println(userPartitionMaxMap.get(true));	// User3
 		System.out.println(userPartitionMaxMap.get(false)); // User5
 
+		Function<Integer, Boolean> function = new Function<Integer, Boolean>(){
+			public Boolean apply(Integer input) {
+				return IntStream.rangeClosed(2, (int) Math.sqrt(input)).noneMatch(i -> input%i==0);
+			}
+		};
+		Map<Boolean, List<Integer>> primes = IntStream.rangeClosed(2, 20).boxed().collect(Collectors.partitioningBy(i -> function.apply(i)));
+		System.out.println(primes.get(true));	// [2, 3, 5, 7, 11, 13, 17, 19]
 		
 		System.out.println("\n>> 6.5 Collector 인터페이스");
 		
-		// 커밋 테스트
-		// 테스트
-		// 테스트
+		Collector<User, List<User>, List<User>> collector = new Collector<User, List<User>, List<User>>() {
+			@Override
+			// 새로운 결과 컨테이너 생성
+			public Supplier<List<User>> supplier() {
+				return ArrayList::new;
+			}
+
+			@Override
+			// 결과 컨테이너에 요소 추가
+			public BiConsumer<List<User>, User> accumulator() {
+				return List::add;
+			}
+
+			@Override
+			// 두 결과 컨테이너 병합
+			public BinaryOperator<List<User>> combiner() {
+				return (i1, i2) -> {
+					i1.addAll(i2);
+					return i1;
+				};
+			}
+
+			@Override
+			// 최종 변환값을 결과 컨테이너로 적용
+			public Function<List<User>, List<User>> finisher() {
+				return Function.identity();			// 항등함수 : 값 자체가 실제 요소
+			}
+
+			@Override
+			public Set<Characteristics> characteristics() {
+				return Collections.unmodifiableSet(EnumSet.of(
+													Characteristics.UNORDERED
+													,Characteristics.CONCURRENT
+													,Characteristics.IDENTITY_FINISH));
+			}
+		};
+		
+		
+		
+		
+		
 		
 		
 		
