7954769067d1e4fac7a139b73b82ccd32ee7257b
fullcrescent <fullcrescent@naver.com>
2022-08-16 21:15:27 +0900

    2022-08-16 작성중

---
+++ src/tistory/모던_자바_인_액션/chapter10/Chapter10.java
@@ -0,0 +1,17 @@
+package tistory.모던_자바_인_액션.chapter10;
+
+public class Chapter10 {
+
+	public static void main(String[] args) {
+		System.out.println("\n>> 10.1 도메인 전용 언어");
+		
+		
+		System.out.println("\n>> 10.2 최신 자바 API의 작은 DSL");
+		
+		System.out.println("\n>> 10.3 자바로 DSL을 만드는 패턴과 기법");
+		
+		System.out.println("\n>> 10.4 실생활의 자바8 DSL");
+		
+	}
+
+}
