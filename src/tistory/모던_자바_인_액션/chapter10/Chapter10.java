fc7b815f2b4478584f01f868dd5eda6609d673d9
fullcrescent <fullcrescent@naver.com>
2022-08-19 23:01:08 +0900

    2022-08-19 작성중

--- src/tistory/모던_자바_인_액션/chapter10/Chapter10.java
+++ src/tistory/모던_자바_인_액션/chapter10/Chapter10.java
@@ -1,12 +1,31 @@
 package tistory.모던_자바_인_액션.chapter10;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Chapter10 {
 
 	public static void main(String[] args) {
 		System.out.println("\n>> 10.1 도메인 전용 언어");
 		
+		/* 코드의 잡음 */
+		List<String> list = Arrays.asList("111", "222", "333");
+		list.forEach(new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+		});
+		
+		list.forEach(s -> System.out.println(s));
+		
+		list.forEach(System.out::println);
+		
 		
 		System.out.println("\n>> 10.2 최신 자바 API의 작은 DSL");
+		
+		
 		
 		System.out.println("\n>> 10.3 자바로 DSL을 만드는 패턴과 기법");
 		
