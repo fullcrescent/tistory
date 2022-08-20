1aeac46406763844e48c9428f2087691ffc39c77
fullcrescent <fullcrescent@naver.com>
2022-08-20 23:19:18 +0900

    2022-08-20 작성중

--- src/tistory/모던_자바_인_액션/chapter10/Chapter10.java
+++ src/tistory/모던_자바_인_액션/chapter10/Chapter10.java
@@ -1,12 +1,19 @@
 package tistory.모던_자바_인_액션.chapter10;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class Chapter10 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("\n>> 10.1 도메인 전용 언어");
 		
 		/* 코드의 잡음 */
@@ -25,6 +32,26 @@ public class Chapter10 {
 		
 		System.out.println("\n>> 10.2 최신 자바 API의 작은 DSL");
 		
+		List<String> error;
+		error = new ArrayList<>();
+		int errorCount = 0;
+		BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+		String line1 = br.readLine();
+		while(errorCount<5 && line1!=null) {
+			if(line1.startsWith("ERROR")) {
+				error.add(line1);
+				errorCount++;
+			}
+			line1 = br.readLine();
+		}
+		br.close();
+		System.out.println(error);	// [ERROR 1, ERROR 2, ERROR 3, ERROR 4, ERROR 5]
+		
+		error = Files.lines(Paths.get("test.txt"))
+					.filter(line2 -> line2.startsWith("ERROR"))
+					.limit(5)
+					.collect(Collectors.toList());
+		System.out.println(error);	// [ERROR 1, ERROR 2, ERROR 3, ERROR 4, ERROR 5]
 		
 		
 		System.out.println("\n>> 10.3 자바로 DSL을 만드는 패턴과 기법");
