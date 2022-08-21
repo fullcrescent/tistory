7a3119dfaa691be161e8583f8ac647449fd48910
fullcrescent <fullcrescent@naver.com>
2022-08-21 23:54:34 +0900

    2022-08-21 Chapter10 관련 클래스

---
+++ src/tistory/모던_자바_인_액션/chapter10/Order.java
@@ -0,0 +1,23 @@
+package tistory.모던_자바_인_액션.chapter10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+	private String customer;
+	private List<Trade> trades = new ArrayList<>();
+	
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	
+	public void addTrade(Trade trade) {
+		trades.add(trade);
+	}
+	public double getValue() {
+		return trades.stream().mapToDouble(Trade::getValue).sum();
+	}
+}
