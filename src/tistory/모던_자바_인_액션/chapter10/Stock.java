63d2e029aa8c769d5e6f6243aab9d1f8f0b5b482
fullcrescent <fullcrescent@naver.com>
2022-08-21 23:45:41 +0900

    2022-08-21 챕터10 관련 클래스

---
+++ src/tistory/모던_자바_인_액션/chapter10/Stock.java
@@ -0,0 +1,20 @@
+package tistory.모던_자바_인_액션.chapter10;
+
+public class Stock {
+	private String symbol;
+	private String market;
+	
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getMarket() {
+		return market;
+	}
+	public void setMarket(String market) {
+		this.market = market;
+	}
+	
+}
