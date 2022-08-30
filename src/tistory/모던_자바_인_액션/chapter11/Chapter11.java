954dadf5f583bd5370403f20584a68644a38caab
fullcrescent <fullcrescent@naver.com>
2022-08-30 23:04:07 +0900

    2022-08-30 작성중

---
+++ src/tistory/모던_자바_인_액션/chapter11/Chapter11.java
@@ -0,0 +1,26 @@
+package tistory.모던_자바_인_액션.chapter11;
+
+public class Chapter11 {
+
+	public static void main(String[] args) {
+		System.out.println("\n>> 11 null 대신 Optional 클래스");
+		
+		System.out.println("\n>> 11.1 값이 없는 상황을 어떻게 처리할까?");
+		
+		
+		
+		System.out.println("\n>> 11.2 Optional 클래스 소개");
+		
+		
+		
+		System.out.println("\n>> 11.3 Optional 적용 패턴");
+		
+		
+		
+		System.out.println("\n>> 11.4 Optional을 사용한 실용 예제 ");
+		
+		
+		
+	}
+
+}
