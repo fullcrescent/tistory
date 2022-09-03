0504a47d61ac41b8f6c55e77511420b138704efc
fullcrescent <fullcrescent@naver.com>
2022-09-03 22:15:39 +0900

    작성중

--- src/tistory/모던_자바_인_액션/chapter11/Chapter11.java
+++ src/tistory/모던_자바_인_액션/chapter11/Chapter11.java
@@ -1,5 +1,7 @@
 package tistory.모던_자바_인_액션.chapter11;
 
+import java.util.Optional;
+
 public class Chapter11 {
 
 	public static void main(String[] args) {
@@ -7,14 +9,9 @@ public class Chapter11 {
 		
 		System.out.println("\n>> 11.1 값이 없는 상황을 어떻게 처리할까?");
 		
-		
-		
 		System.out.println("\n>> 11.2 Optional 클래스 소개");
 		
-		
-		
 		System.out.println("\n>> 11.3 Optional 적용 패턴");
-		
 		
 		
 		System.out.println("\n>> 11.4 Optional을 사용한 실용 예제 ");
@@ -23,4 +20,29 @@ public class Chapter11 {
 		
 	}
 
+}
+
+/* 객체 중첩 구조 */
+class Person{
+	private Optional<Car> car;
+	
+	public Optional<Car> getCar() {
+		return car;
+	}
+}
+
+class Car{
+	private Optional<Insurance> insurance;
+
+	public Optional<Insurance> getInsurance() {
+		return insurance;
+	}
+}
+
+class Insurance{
+	private String name;		// Optional이 아님은 필수 값을 의미
+	
+	public String getName()	{
+		return name;
+	}
 }
