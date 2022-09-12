d276f9ba9383a924ed02a96dd0a081140e355c9b
fullcrescent <fullcrescent@naver.com>
2022-09-12 15:31:34 +0900

    작성중

---
+++ src/tistory/모던_자바_인_액션/chapter13/Chapter13.java
@@ -0,0 +1,28 @@
+package tistory.모던_자바_인_액션.chapter13;
+
+public class Chapter13 {
+
+	public static void main(String[] args) {
+		System.out.println("\n>> 13 디폴트 메서드");
+		
+		
+		
+		System.out.println("\n>> 13.1 변화하는 API");
+		
+		
+		
+		System.out.println("\n>> 13.2 디폴트 메서드란 무엇인가?");
+		
+		
+		
+		System.out.println("\n>> 13.3 디폴트 메서드 활용 패턴");
+		
+		
+		
+		System.out.println("\n>> 13.4 해석 규칙");
+		
+		
+		
+	}
+
+}
