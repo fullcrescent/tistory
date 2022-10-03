7081aaadd843e763755c8a3b547eed732c34e23c
fullcrescent <fullcrescent@naver.com>
2022-10-03 16:21:45 +0900

    작성중

---
+++ src/tistory/모던_자바_인_액션/chapter14/Chapter14.java
@@ -0,0 +1,21 @@
+package tistory.모던_자바_인_액션.chapter14;
+
+public class Chapter14 {
+	
+	public static void main(String[] args) {
+		System.out.println("\n>> 14 자바 모듈 시스템");
+		
+		System.out.println("\n>> 14.1 압력 : 소프트웨어 유추");
+		System.out.println("\n>> 14.2 자바 모듈 시스템을 설계한 이유");
+		System.out.println("\n>> 14.3 자바 모듈 : 큰 그림");
+		System.out.println("\n>> 14.4 자바 모듈 시스템으로 애플리케이션 개발하기");
+		System.out.println("\n>> 14.5 여러 모듈 활용하기");
+		System.out.println("\n>> 14.6 컴파일과 패키징");
+		System.out.println("\n>> 14.7 자동 모듈");
+		System.out.println("\n>> 14.8 모듈 정의와 구문들");
+		System.out.println("\n>> 14.9 더 큰 예제 그리고 더 배울 수 있는 방법");
+		
+
+		
+	}
+}
