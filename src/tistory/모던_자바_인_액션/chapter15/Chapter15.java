06340b5a0eb679a434c0c816f9849483676b6b1f
fullcrescent <fullcrescent@naver.com>
2022-10-07 21:44:09 +0900

    작성중

---
+++ src/tistory/모던_자바_인_액션/chapter15/Chapter15.java
@@ -0,0 +1,16 @@
+package tistory.모던_자바_인_액션.chapter15;
+
+public class Chapter15 {
+
+	public static void main(String[] args) {
+		System.out.println("\n>> 15 CompletableFuture와 리액티브 프로그래밍 컨셉의 기초");
+		
+		System.out.println("\n>> 15.1 동시성을 구현하는 자바 지원의 진화");
+		System.out.println("\n>> 15.2 동기 API와 비동기 API");
+		System.out.println("\n>> 15.3 박스와 채널 모델");
+		System.out.println("\n>> 15.4 CompletableFuture와 콤비네이터를 이용한 동시성");
+		System.out.println("\n>> 15.5 발행-구독 그리고 리액티브 프로그래밍");
+		System.out.println("\n>> 15.6 리액티브 시스템 vs 리액티브 프로그래밍");
+	}
+
+}
