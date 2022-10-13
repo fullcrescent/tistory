1c71dc3ab5d9007abf79feb64d76d183cf7738df
fullcrescent <fullcrescent@naver.com>
2022-10-13 22:52:30 +0900

    작업중

---
+++ src/tistory/모던_자바_인_액션/chapter16/Chapter16.java
@@ -0,0 +1,15 @@
+package tistory.모던_자바_인_액션.chapter16;
+
+public class Chapter16 {
+
+	public static void main(String[] args) {
+		System.out.println("\n>> 16 CompletableFuture : 안정적 비동기 프로그래밍");
+		
+		System.out.println("\n>> 16.1 Future의 단순 활용");
+		System.out.println("\n>> 16.2 비동기 API 구현");
+		System.out.println("\n>> 16.3 비블록 코드 만들기");
+		System.out.println("\n>> 16.4 비동기 작업 파이프라인 만들기");
+		System.out.println("\n>> 16.5 CompletableFuture의 종료에 대응하는 방법");
+	}
+
+}
