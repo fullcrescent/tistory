611a1c829b5ff632b9f6c3049f89a647e71fd442
fullcrescent <fullcrescent@naver.com>
2022-10-14 21:14:56 +0900

    작성중

--- src/tistory/모던_자바_인_액션/chapter16/Chapter16.java
+++ src/tistory/모던_자바_인_액션/chapter16/Chapter16.java
@@ -1,5 +1,9 @@
 package tistory.모던_자바_인_액션.chapter16;
 
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
 public class Chapter16 {
 
 	public static void main(String[] args) {
@@ -12,4 +16,46 @@ public class Chapter16 {
 		System.out.println("\n>> 16.5 CompletableFuture의 종료에 대응하는 방법");
 	}
 
+}
+
+class Util {
+	public static void delay() {
+		try {
+			Thread.sleep(1000L);
+		}catch (InterruptedException e) {
+			throw new RuntimeException();
+		}
+	}
+}
+
+class Shop {
+	private final String name;
+	private final Random random;
+	
+	public Shop(String name) {
+		this.name = name;
+		random = new Random(name.charAt(0) * name.charAt(1) * name.charAt(2));
+	}
+	
+	public double getPrice(String product) {
+		return calculatePrice(product);
+	}
+
+	private double calculatePrice(String product) {
+		Util.delay();
+		return random.nextDouble() * product.charAt(0) + product.charAt(1);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Future<Double> getPriceAsync(String product) {
+		CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+		new Thread(() -> {
+			double price = calculatePrice(product);
+			futurePrice.complete(price);
+		}).start();
+		return futurePrice;
+	}
 }
