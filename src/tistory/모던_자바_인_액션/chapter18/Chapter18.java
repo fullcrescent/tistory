0c59f4d9a3d54cfb5afa47cedbea2455afd17257
fullcrescent <fullcrescent@naver.com>
2022-10-27 23:04:50 +0900

    작성중

---
+++ src/tistory/모던_자바_인_액션/chapter18/Chapter18.java
@@ -0,0 +1,12 @@
+package tistory.모던_자바_인_액션.chapter18;
+
+public class Chapter18 {
+
+	public static void main(String[] args) {
+		System.out.println("\n>> 18 함수형 관점으로 생각하기");
+		System.out.println("\n>> 18.1 시스템 구현과 유지보수");
+		System.out.println("\n>> 18.2 함수형 프로그래밍이란 무엇인가?");
+		System.out.println("\n>> 18.3 재귀와 반복");
+	}
+
+}
