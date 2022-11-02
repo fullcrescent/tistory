6cb3f96d4934b4a4b8c7faf436a6d2f677e78e84
fullcrescent <fullcrescent@naver.com>
2022-11-02 21:11:33 +0900

    작성중

---
+++ src/tistory/모던_자바_인_액션/chapter19/Chapter19.java
@@ -0,0 +1,17 @@
+package tistory.모던_자바_인_액션.chapter19;
+
+public class Chapter19 {
+	public static void main(String[] args) {
+		System.out.println("\n>> 19 함수형 프로그래밍 기법");
+		System.out.println("\n>> 19.1 함수는 모든 곳에 존재한다");
+		
+		System.out.println("\n>> 19.2 영속 자료구조");
+		
+		System.out.println("\n>> 19.3 스트림과 게으른 평가");
+		
+		System.out.println("\n>> 19.4 패턴 매칭");
+		
+		System.out.println("\n>> 19.5 기타 정보");
+		
+	}
+}
